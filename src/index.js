16be27e690ddf5cf736490ffd5874dcdf7c7ab08
ajhanson22227 <ajhanson22227@gmail.com>
2020-09-07 15:31:20 -0500

    Add page load with webpack

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-console.log("Hello Werld");
+import loadProjectPage from './projects'
+
+const main_container = document.getElementById('container');
+
+function loadProject() {
+	while (main_container.firstChild) main_container.removeChild(main_container.firstChild);
+	loadProjectPage();
+}
+
+loadProject();
